de83da73d93202897f3538f0cbf0b1ab4b4a56a4
Michael <mickmick117@hotmail.com>
2019-01-14 18:44:28 -0500

    test ctrl

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPNETCoreHeroku.Controllers
+{
+    [Route("yolo")]
+    [ApiController]
+    public class HomeController1 : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "test";
+        }
+    }
+}
